f8def536c11df107a76b91b42c978eef5f55cb02
billchetch <bill@chetch.net>
2020-07-07 21:07:15 +0800

    Initial commit

---
+++ app/src/main/java/net/chetch/appresourceslib/MainActivity.java
@@ -0,0 +1,27 @@
+package net.chetch.appresourceslib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar)findViewById(R.id.actionbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        return true;
+    }
+}
